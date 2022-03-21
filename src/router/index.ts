3e14746f35b6e34704d0750da55928fc9e9a3ab9
forbidden Betta <forbiddenBetta@gmail.com>
2022-03-21 15:40:36 +0100

    Work 2

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/articles",
+      name: "articles",
+      component: () => import("../views/ArticlesView.vue"),
+    },
   ],
 });
 
---
+++ src/views/ArticlesView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="articles">
+    <h1>liste articles</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            articles:[]
+        }
+    },
+    mounted(){
+      this.fetchArticles()
+    },
+    methods : {
+      async fetchARticles(){
+        let response = await fetch("https://api.blog.quidam.re/api/getArticles.php")
+          .then(reponse => response.json())
+          .catch(e => e)
+        if (articles instanceof Array) {
+          this.articles = articles;
+        }
+      },
+
+
+      // fetchArticles(){
+      //   fetch("https://api.blog.quidam.re/api/getArticles.php")
+      //   .then( (response) => {
+      //     console.log(response);
+      //   }).catch( (error) => {
+      //     console.warning(error);
+      //   });
+      // },
+    },
+};
+</script>
+
+<style scoped></style>
