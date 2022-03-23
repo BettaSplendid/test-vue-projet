a9667ef3d7b9049607fa4ae7779c3f417ca65a51
forbidden Betta <forbiddenBetta@gmail.com>
2022-03-23 15:27:02 +0100

    I SPLIT THIS COMMIT IN HALF

---
+++ src/components/AddArticle.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>This is an about page</h1>
+  Vous êtes dans radio : AddArticle.vue
+
+  <form @submit.prevent="$emit('handleSubmit')">
+    <p>categorie is: {{ user.categorie }}</p>
+    <input
+      type="text"
+      v-model="user.categorie"
+      name="categorie"
+      id="categorie"
+      placeholder="categorie"
+    />
+    <input
+      type="text"
+      v-model="user.title"
+      name="title"
+      id="title"
+      placeholder="title"
+    />
+    <input
+      type="text"
+      v-model="user.content"
+      name="content"
+      id="content"
+      placeholder="content"
+    />
+    <input
+      type="number"
+      v-model="user.user_id"
+      name="user_id"
+      id="user_id"
+      placeholder="user_id"
+    />
+    <input type="submit" value="submit" />
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+};
+</script>
--- src/views/AddArticleView.vue
+++ src/views/AddArticleView.vue
@@ -1,43 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <form @submit.prevent="handleSubmit">
-      <p>Message is: {{ user.categorie }}</p>
-      <input
-        type="text"
-        v-model="user.categorie"
-        name="categorie"
-        id="categorie"
-        placeholder="categorie"
-      />
-      <input
-        type="text"
-        v-model="user.title"
-        name="title"
-        id="title"
-        placeholder="title"
-      />
-      <input
-        type="text"
-        v-model="user.content"
-        name="content"
-        id="content"
-        placeholder="content"
-      />
-      <input
-        type="number"
-        v-model="user.user_id"
-        name="user_id"
-        id="user_id"
-        placeholder="user_id"
-      />
-      <input type="submit" value="submit" />
-    </form>
+  <div>
+      Vous êtes dans radio : AddArticleView.vue
+      <AddArticle @handleSubmit="handleSubmit" :user="user" />
   </div>
 </template>
 
 <script>
 
+import AddArticle from "../components/AddArticle.vue";
 export default {
   data() {
     return {
@@ -48,6 +18,9 @@ export default {
         user_id: "",
       },
     };
+  },
+  components: {
+    AddArticle,
   },
   methods: {
     async handleSubmit() {
