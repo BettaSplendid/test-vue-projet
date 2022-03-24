72c18ccbbbae6c5bae96bab32d1d792411f94b32
forbidden Betta <forbiddenBetta@gmail.com>
2022-03-24 16:28:18 +0100

    Done

--- src/components/export_api.js
+++ src/components/export_api.js
@@ -1,18 +1,18 @@
 import { useArticleStore } from "../stores/articles_store.ts";
 
 export async function limport() {
-    const storeStore = useArticleStore();
-    let collected_articles = await fetch(
-            "https://api.blog.quidam.re/api/getArticles.php"
-        )
-        .then((response) => response.json())
-        .catch((e) => e);
-    if (collected_articles instanceof Array) {
-        console.log("Collected articles check");
-        let articles = {};
-        articles.value = collected_articles;
-    }
+  const storeStore = useArticleStore();
+  let collected_articles = await fetch(
+    "https://api.blog.quidam.re/api/getArticles.php"
+  )
+    .then((response) => response.json())
+    .catch((e) => e);
+  if (collected_articles instanceof Array) {
+    console.log("Collected articles check");
+    let articles = {};
+    articles.value = collected_articles;
+  }
 
-    storeStore.articles = collected_articles;
-    console.log(storeStore.articles);
+  storeStore.articles = collected_articles;
+  console.log(storeStore.articles);
 }
